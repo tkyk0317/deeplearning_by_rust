11cdbf08b038d8acf2acf7d69b8e2a79f3c9fb38
tanaka takayuki <t_tanaka@sports-f.co.jp>
2017-08-15 10:13:01 +0900

    [add] ステップ関数とReLU関数追加.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate gnuplot;
 extern crate ndarray;
 
-use gnuplot::{Figure, Caption, Color};
+use gnuplot::{Figure, Caption, Color, AxesCommon, Fix};
 use ndarray::{Array1};
 
 // ニューロン構造体.
@@ -13,14 +13,24 @@ struct Neuron {
 
 // ニューロン実装.
 impl Neuron {
-    pub fn new(bias: f64, data: Array1<f64>, weight: Array1<f64>) -> Neuron {
-        Neuron { bias: bias, data: data, weight: weight }
+    pub fn new(bias: f64, data: Array1<f64>, weight: Array1<f64>) -> Option<Neuron> {
+        if data.len() != weight.len() {
+            return None;
+        }
+        return Some(Neuron { bias: bias, data: data, weight: weight });
     }
 
-    // パーセプトロン.
-    fn perceptron(&self) -> f64 {
-        let _n = self.data.clone() * self.weight.clone() + self.bias.clone();
-        _n.iter().fold(Array1::zeros(1), |acc, &x| acc + x)[0]
+    // 内積.
+    fn dot(&self) -> f64 {
+        self.data.dot(&self.weight) + self.bias
+    }
+}
+
+// ステップ関数.
+fn step(x: f64) -> u64 {
+    match x > 0. {
+        true  => 1,
+        false => 0,
     }
 }
 
@@ -29,21 +39,37 @@ fn sigmoid(x: f64) -> f64 {
     1. / (1. + (-x).exp())
 }
 
+// relu関数.
+fn relu(x: f64) -> f64 {
+    match x > 0. {
+        true  => x,
+        false => 0.
+    }
+}
+
 fn main() {
     let mut n = Array1::<f64>::zeros(4);
     let mut w = Array1::<f64>::zeros(4);
     n[0] = 10.;
     w[0] = 2.;
-    let _n = Neuron::new(10., n, w);
-    println!("{:?}", _n.perceptron());
-    println!("{}", sigmoid(_n.perceptron()));
+    let _n = Neuron::new(10., n, w).unwrap();
 
-    // シグモイド関数描画.
-    let x: Vec<f64> = vec![-10., -9., -8., -7., -6., -5., -4., -3., -2., -1.,
-                           0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10.];
-    // haskellの[m..n]みたいな書き方はできないのだろうか？？？
-    let y: Vec<f64> = x.iter().map(|i| sigmoid(*i)).collect();
+    println!("{:?}", _n.dot());
+    println!("{}", sigmoid(_n.dot()));
+
+    // 活性化関数適用.
+    let mut x: Vec<f64> = std::vec::Vec::new();
+    for i in -100..100 { x.push((i as f64) / 10.); }
+    let sig: Vec<f64> = x.iter().map(|i| sigmoid(*i)).collect(); // sigmoid function.
+    let step: Vec<u64> = x.iter().map(|i| step(*i)).collect(); // step function.
+    let relu: Vec<f64> = x.iter().map(|i| relu(*i)).collect(); // relu function.
+
+    // グラフ描画.
     let mut fg = Figure::new();
-    fg.axes2d().lines(&x, &y, &[]);
+    fg.axes2d()
+        .lines(&x, &sig,  &[Caption("Sigmoid"), Color("red")])
+        .lines(&x, &step, &[Caption("Step"), Color("blue")])
+        .lines(&x, &relu, &[Caption("ReLU"), Color("green")])
+        .set_y_range(Fix(-0.5), Fix(2.0));
     fg.show();
 }
