1160ce10be6d03d37cc6f7bd9cfd6207e95881aa
tanaka takayuki <t_tanaka@sports-f.co.jp>
2017-08-16 17:48:40 +0900

    [add] 損失関数追加.
    二剰誤差関数と交差エントロピー関数追加（動作確認はまだ）.

--- src/neuron.rs
+++ src/neuron.rs
@@ -53,3 +53,21 @@ impl<'a> Neuron<'a> {
         _dot.map(|i| { (i - _max).exp() / _sum })
     }
 }
+
+// 損失関数.
+pub fn sum_of_square(result: DMatrix<f64>, reference: DMatrix<f64>) -> f64 {
+    result.iter()
+          .zip(reference.iter())
+          .fold(0., |acc, (_result, _reference)| {
+              acc + (_result - _reference).powi(2)
+          }) / 2.
+}
+
+// 交差エントロピー関数.
+pub fn cross_entropy(result: DMatrix<f64>, reference: DMatrix<f64>) -> f64 {
+    -1. * result.iter()
+                .zip(reference.iter())
+                .fold(0., |acc, (_result, _reference)| {
+                    acc + _result + _reference.ln()
+                })
+}
