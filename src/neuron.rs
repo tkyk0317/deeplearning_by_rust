f11a4216e647ce2ac2e3ed9c0a2bad8b1d24784d
tanaka takayuki <t_tanaka@sports-f.co.jp>
2017-08-17 11:31:48 +0900

    [modify] 交差エントロピー関数でマイナス無限大対応追加.
    ln(0)の場合、マイナス無限大が発生する為、微小な値を加算するよう、修正.

--- src/neuron.rs
+++ src/neuron.rs
@@ -55,19 +55,20 @@ impl<'a> Neuron<'a> {
 }
 
 // 損失関数.
-pub fn sum_of_square(result: DMatrix<f64>, reference: DMatrix<f64>) -> f64 {
+pub fn sum_of_square(result: &DMatrix<f64>, reference: &DMatrix<f64>) -> f64 {
     result.iter()
           .zip(reference.iter())
           .fold(0., |acc, (_result, _reference)| {
               acc + (_result - _reference).powi(2)
-          }) / 2.
+          }) * 0.5
 }
 
 // 交差エントロピー関数.
-pub fn cross_entropy(result: DMatrix<f64>, reference: DMatrix<f64>) -> f64 {
+pub fn cross_entropy(result: &DMatrix<f64>, reference: &DMatrix<f64>) -> f64 {
     -1. * result.iter()
                 .zip(reference.iter())
                 .fold(0., |acc, (_result, _reference)| {
-                    acc + _result + _reference.ln()
+                    // マイナス無限大が発生しないよう、小さな値を加算(ln(0)＝-inf).
+                    acc + _reference * (_result + 1.0e-7).ln()
                 })
 }
